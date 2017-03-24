e183a814498aa757800c5732d7b51562eafa00c8
svilen <svilen@email>
2017-03-24 13:58:07 +0200

    setp

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function #,unicode_literals
+from setuptools import setup
+
+setup(
+    name='svd_util',
+    version='123',
+    description='python utility funcs, classes, small languages+frameworks, scripts.',
+    url='https://github.com/svilendobrev/svd_util',
+    author='svilen dobrev',
+    license='MIT',
+    packages=[
+        'svd_util',
+    ],
+)
+# vim:ts=4:sw=4:expandtab
